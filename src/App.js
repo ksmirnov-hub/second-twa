1a74f80b631e7fc7d9a66a45cc208897b3509b71
smirnov.kons <smirnov.kons@jilfond.ru>
2024-09-29 07:45:02 +0300

    img

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Logo from './images/logoIcon.png';
 const App = () => {
 
   return (
     <div className="app">
        <div class="center-block">
-
+          <div>
+            <img width='211px' height='202px' src={Logo} alt='' />
+          </div>
        </div>
     </div>
   );
