c74207705f38efc336bc85e367a1a13813b44312
smirnov.kons <smirnov.kons@jilfond.ru>
2024-10-01 10:28:13 +0300

    ways fix

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import AppLayout from './layouts/AppLayout';
-import Start from 'pages/start/start';
-import Home from 'pages/home';
-import Account from 'pages/account';
+import Start from './pages/start/start';
+import Home from './pages/home';
+import Account from './pages/account';
 
 const App = () => {
   return (
