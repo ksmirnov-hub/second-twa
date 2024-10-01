8e2fe4ccbb400060c8f500ef486d643decd9cabb
smirnov.kons <smirnov.kons@jilfond.ru>
2024-10-01 07:41:41 +0300

    111111111

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import AppLayout from 'layouts/AppLayout';
 
-import Start from 'pages/start/start';
-import Home from 'pages/home';
-import Account from 'pages/account';
-
 const App = () => {
   return (
     <Routes>
