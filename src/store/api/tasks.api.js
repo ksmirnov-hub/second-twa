ec1852d30ab636062743bd7f074bcf0f3813f0f7
smirnov.kons <smirnov.kons@jilfond.ru>
2024-10-20 14:19:44 +0300

    tasks fix

--- src/store/api/tasks.api.js
+++ src/store/api/tasks.api.js
@@ -4,7 +4,7 @@ import baseQueryWithToken from './helpers/customFetchBase';
 // const HEALTHAPP_URI = process.env.REACT_APP_BASE_URL;
 const WEPAPP_URI = 'https://api.life-coin.ru';
 
-const TASKS = '/api/tasks/all';
+const TASKS = '/api/tasks';
 const TASK = '/api/task';
 
 export const tasksApi = createApi({
@@ -17,7 +17,7 @@ export const tasksApi = createApi({
                 let user = window.Telegram?.WebApp?.initDataUnsafe?.user?.id || '316601649';
                 let parameters = ''
                 if (categoryId) {
-                    parameters = '?user=' + user + '&category=' + categoryId + 'is_completed=0'
+                    parameters = '?user=' + user + '&category=' + categoryId + '&is_completed=0'
                 }
 				return {
                     url: WEPAPP_URI + TASKS + parameters,
