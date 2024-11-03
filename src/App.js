072ee2cf8368567ef893cf40ab58714251586f35
smirnov.kons <smirnov.kons@jilfond.ru>
2024-11-03 04:59:24 +0300

    temp

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
     if (profile.id) {
       setTimeout(() => {
         setStartPage(isNewUser ? <Start /> : <Home />)
-      }, 3000)
+      }, 3000000)
     }
   }, [profile])
 
