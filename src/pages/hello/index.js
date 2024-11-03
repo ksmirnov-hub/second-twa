6f3e6ec0c91eae41c5eae73e9b36c1f439e1b7b1
smirnov.kons <smirnov.kons@jilfond.ru>
2024-11-03 04:55:09 +0300

    main page fix

--- src/pages/hello/index.js
+++ src/pages/hello/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { motion } from "framer-motion"
 import { useNavigate  } from 'react-router-dom';
 import Logo from '../../images/logo-home.png';
+import LogoIcon from '../../images/logo';
 import './hello.css';
 
 
@@ -9,9 +11,13 @@ const Home = () => {
   return (
     <div className="hello">
        <div className="center-block">
-          <div className="img">
-            <img width='211px' height='202px' src={Logo} alt='' />
-          </div>
+        <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <LogoIcon />
+          </motion.div>
           <div className="hello-title">
             Life Coin
           </div>
