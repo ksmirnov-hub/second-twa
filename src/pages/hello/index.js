4db6e4fa555ff1b12e9c52ac5351d27740ed533b
smirnov.kons <smirnov.kons@jilfond.ru>
2024-11-03 06:10:06 +0300

    animation

--- src/pages/hello/index.js
+++ src/pages/hello/index.js
@@ -9,12 +9,21 @@ import './hello.css';
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div className="hello">
+    <motion.div
+      className="hello"
+      initial={{
+        background: 'radial-gradient(50.56% 70.56% at 70% 38.03%, #40E48F 0%, #00B858 100%)'
+      }}
+      animate={{
+        background: 'radial-gradient(70.56% 70.56% at 50% 38.03%, #40E48F 0%, #00B858 100%)'
+      }}
+      transition={{ duration: 0.7 }}
+    >
        <div className="center-block">
         <motion.div
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 0.6 }}
             className="img"
           >
             <LogoIcon />
@@ -26,7 +35,7 @@ const Home = () => {
             LIFE - это будущее, где время - это самая ценная валюта
           </div>
        </div>
-    </div>
+    </motion.div>
   );
 };
 
